656e83ced6c47f04951b61364f9ef5b2f22f1a6b
merouane meriem <lm_merouane@esi.dz>
2024-03-26 21:02:54 +0100

    Update models.py
    Final_version of models suitable with bdd

--- accounts/models.py
+++ accounts/models.py
@@ -3,25 +3,37 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.utils.translation import gettext_lazy as _
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth.models import Group
+from django.utils import timezone
 
 
 from accounts.managers import UserManager
 
 # Create your models here.
 
-class Role(models.TextChoices):
-    Admin = 'admin', 'Admin'
-    Assistant = 'assistant', 'Assistant'
-    Chercheur = 'chercheur', ' Chercheur' # New role added
-    User = 'user', 'User'
 
-AUTH_PROVIDERS ={'email':'email', 'google':'google', 'github':'github', 'linkedin':'linkedin'}
 
 class User(AbstractBaseUser, PermissionsMixin):
+    ADMIN = 'admin'
+    ASSISTANT = 'assistant'
+    CHERCHEUR = 'chercheur'
+    USER = 'user'
+
+    ROLE_CHOICES = [
+        (ADMIN, 'Admin'),
+        (ASSISTANT, 'Assistant'),
+        (CHERCHEUR, 'Chercheur'),
+        (USER, 'User'),
+    ]
+
+    AUTH_PROVIDERS = {
+        'email': 'email',
+        'google': 'google',
+        'github': 'github',
+        'linkedin': 'linkedin'
+    }
+
     id = models.BigAutoField(primary_key=True, editable=False)
-    email = models.EmailField(
-        max_length=255, verbose_name=_("Email Address"), unique=True
-    )
+    email = models.EmailField(max_length=255, verbose_name=_("Email Address"), unique=True)
     first_name = models.CharField(max_length=100, verbose_name=_("First Name"))
     last_name = models.CharField(max_length=100, verbose_name=_("Last Name"))
     is_staff = models.BooleanField(default=False)
@@ -30,15 +42,15 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(auto_now_add=True)
     last_login = models.DateTimeField(auto_now=True)
-    role = models.CharField(_('role'), max_length=20, choices=Role.choices, default=Role.User)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, null=True)
     auth_provider = models.CharField(max_length=50, blank=False, null=False, default=AUTH_PROVIDERS.get('email'))
 
-
     USERNAME_FIELD = "email"
-
     REQUIRED_FIELDS = ["first_name", "last_name"]
-
     objects = UserManager()
+
+
+
 
     def tokens(self):
         refresh = RefreshToken.for_user(self)
@@ -56,12 +68,9 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def assign_role(self, role_name):
         # Make sure the role name is valid
-        if role_name in dict(Role.choices):
-            # Assign the user to the appropriate group based on the role
-            group, _ = Group.objects.get_or_create(name=role_name)
-            self.groups.add(group)
-            # Update the role attribute
+        if role_name in dict(self.ROLE_CHOICES).keys():
             self.role = role_name
             self.save()
         else:
             raise ValueError("Invalid role name")
+
