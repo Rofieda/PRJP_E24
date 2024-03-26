e18a00c45f43a5675487d6298cfbea01fdc7c85d
merouane meriem <lm_merouane@esi.dz>
2024-03-26 21:04:43 +0100

    Update views.py
    final_version

--- accounts/views.py
+++ accounts/views.py
@@ -8,8 +8,8 @@ from rest_framework.exceptions import PermissionDenied
 from django.core.mail import send_mail
 
 
-from accounts.serializers import UserRegisterSerializer, LoginSerializer, \
-    PasswordResetRequestSerializer, SetNewPasswordSerializer, AddUserSerializer, UserSerializer
+from accounts.serializers import UserRegisterSerializer, LoginSerializer, AddUserSerializer, UserSerializer, \
+    PasswordResetRequestSerializer, SetNewPasswordSerializer
 from rest_framework import status
 from django.utils.http import urlsafe_base64_decode
 from django.utils.encoding import smart_str, DjangoUnicodeDecodeError
@@ -18,17 +18,12 @@ from rest_framework.permissions import IsAuthenticated
 from .models import User
 from rest_framework.generics import DestroyAPIView
 from .permissions import IsAdminUser
-
-
-
 class RegisterView(GenericAPIView):
     serializer_class = UserRegisterSerializer
 
     def post(self, request):
-        user = request.data
-        serializer = self.serializer_class(data=user)
+        serializer = self.serializer_class(data=request.data)
         if serializer.is_valid(raise_exception=True):
-            role = request.data.get('role')
             user = serializer.save()
 
             # Send email notification
@@ -43,10 +38,10 @@ class RegisterView(GenericAPIView):
 
     def send_signup_email(self, user):
         subject = 'Bienvenue à LMCS'
-        full_name = f"{user.first_name} {user.last_name}"
+        full_name = f"{user.first_name} {user.last_name}"  # Access attributes using dot notation
         message = f'Bonjour {full_name},\n\nVous avez été ajouté au labo LMCS avec succès.'
         from_email = 'your_email@example.com'  # Set your sender email here
-        to_email = user.email
+        to_email = user.email  # Access email attribute using dot notation
         send_mail(subject, message, from_email, [to_email])
 
 
@@ -61,54 +56,7 @@ class LoginUserView(GenericAPIView):
         # Add the role to the response data
         response_data = serializer.data
         response_data['role'] = role
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-class PasswordResetRequestView(GenericAPIView):
-    serializer_class = PasswordResetRequestSerializer
-
-    def post(self, request):
-        serializer = self.serializer_class(data=request.data, context={'request': request})
-        serializer.is_valid(raise_exception=True)
-        return Response({'message': 'we have sent you a link to reset your password'}, status=status.HTTP_200_OK)
-        # return Response({'message':'user with that email does not exist'}, status=status.HTTP_400_BAD_REQUEST
-
-
-class PasswordResetConfirm(GenericAPIView):
-
-    def get(self, request, uidb64, token):
-        try:
-            user_id=smart_str(urlsafe_base64_decode(uidb64))
-            user=User.objects.get(id=user_id)
-
-            if not PasswordResetTokenGenerator().check_token(user, token):
-                return Response({'message':'token is invalid or has expired'}, status=status.HTTP_401_UNAUTHORIZED)
-            return Response({'success':True, 'message':'credentials is valid', 'uidb64':uidb64, 'token':token}, status=status.HTTP_200_OK)
-
-        except DjangoUnicodeDecodeError as identifier:
-            return Response({'message':'token is invalid or has expired'}, status=status.HTTP_401_UNAUTHORIZED)
-
-
-class SetNewPasswordView(GenericAPIView):
-    serializer_class=SetNewPasswordSerializer
-
-    def patch(self, request):
-        serializer=self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        return Response({'success':True, 'message':"password reset is succesful"}, status=status.HTTP_200_OK)
-
-
-class TestingAuthenticatedReq(GenericAPIView):
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        data = {
-            'msg': 'its works'
-        }
-        return Response(data, status=status.HTTP_200_OK)
-
-
-
+        return Response(response_data, status=status.HTTP_200_OK)
 
 class LogoutAPIView(APIView):
     def post(self, request):
@@ -118,8 +66,7 @@ class LogoutAPIView(APIView):
 
         # Optionally, you can also perform other cleanup tasks
 
-        return Response({"detail": "Logout successful."}, status=status.HTTP_200_OK)
-
+        return Response({"detail": "Déconnexion réussie.."}, status=status.HTTP_200_OK)
 
 
 
@@ -144,12 +91,11 @@ class AddUserView(CreateAPIView):
 
     def send_email_to_user(self, user):
         subject = 'Bienvenue à LMCS'
-        full_name = f"{user.first_name} {user.last_name}"
-        message = f'Bonjour {full_name},\n\nVous avez été ajouté au labo LMCS avec succès.'
-        from_email = 'votre_email@example.com'  # Remplacez par votre adresse e-mail
-        to_email = user.email
+        full_name = f"{user.first_name} {user.last_name}"  # Access attributes using dot notation
+        message = f'Bonjour {full_name},\n\nVous avez été ajouté à LMCSQUEST avec succès. Pour vous connecter, utilisez simplement cette adresse e-mail et utilisez les deux premières lettres de votre prénom comme mot de passe : LMCS_lesdeuxpremiereslettredevotreprenomenminiscule.'
+        from_email = 'your_email@example.com'  # Set your sender email here
+        to_email = user.email  # Access email attribute using dot notation
         send_mail(subject, message, from_email, [to_email])
-
 
 class ListUsersView(APIView):
     permission_classes = [IsAuthenticated]
@@ -167,7 +113,8 @@ class ListUsersView(APIView):
 
 class DeleteUserView(DestroyAPIView):
     queryset = User.objects.all()
-    serializer_class = UserSerializer  # Set the serializer class for this view
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
 
     def delete(self, request, *args, **kwargs):
         try:
@@ -175,9 +122,43 @@ class DeleteUserView(DestroyAPIView):
             # Check if the user sending the request is a superuser or has an admin role
             if request.user.is_superuser or (request.user.role == 'admin' and request.user != user):
                 user.delete()
-                return Response({"detail": "User deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
+                return Response({"detail": "L'utilisateur a été supprimé avec succès."}, status=status.HTTP_204_NO_CONTENT)
             else:
-                return Response({"detail": "You do not have permission to delete this user."},
+                return Response({"detail": "Vous n'avez pas la permission de supprimer cet utilisateur."},
                                 status=status.HTTP_403_FORBIDDEN)
         except User.DoesNotExist:
-            return Response({"detail": "User not found."}, status=status.HTTP_404_NOT_FOUND)
+            return Response({"detail": "Utilisateur non trouvé."}, status=status.HTTP_404_NOT_FOUND)
+
+
+class PasswordResetRequestView(GenericAPIView):
+    serializer_class = PasswordResetRequestSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        return Response({'message': 'Nous vous avons envoyé un lien pour réinitialiser votre mot de passe'}, status=status.HTTP_200_OK)
+        # return Response({'message':'user with that email does not exist'}, status=status.HTTP_400_BAD_REQUEST
+
+
+class PasswordResetConfirm(GenericAPIView):
+
+    def get(self, request, uidb64, token):
+        try:
+            user_id=smart_str(urlsafe_base64_decode(uidb64))
+            user=User.objects.get(id=user_id)
+
+            if not PasswordResetTokenGenerator().check_token(user, token):
+                return Response({'message':'token is invalid or has expired'}, status=status.HTTP_401_UNAUTHORIZED)
+            return Response({'success':True, 'message':'credentials is valid', 'uidb64':uidb64, 'token':token}, status=status.HTTP_200_OK)
+
+        except DjangoUnicodeDecodeError as identifier:
+            return Response({'message':'token is invalid or has expired'}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+class SetNewPasswordView(GenericAPIView):
+    serializer_class=SetNewPasswordSerializer
+
+    def patch(self, request):
+        serializer=self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response({'success':True, 'message':"La réinitialisation du mot de passe a réussi"}, status=status.HTTP_200_OK)
